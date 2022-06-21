674dbe978ef4f0419c27edd1f4d31d13fdc63af7
Deniz Bozdas <dbozdas@gmail.com>
2022-06-21 08:13:18 -0500

    commit

--- src/main/java/com/solvd/deliveryservice/practise_api/API.java
+++ src/main/java/com/solvd/deliveryservice/practise_api/API.java
@@ -1,4 +1,4 @@
-package com.solvd.deliveryservice.api;
+package com.solvd.deliveryservice.practise_api;
 
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
@@ -7,7 +7,7 @@ import java.net.InetSocketAddress;
 
 public class API {
     public static void main(String[] args) throws IOException {
-
+//
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/api/greeting", (exchange -> {
 
